a80fada80e935a171c52bf8ac7f487786d798f85
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2024-08-23 20:18:03 +0300

    Updated server.js

--- server.js
+++ server.js
@@ -22,8 +22,7 @@ const DB = process.env.DATABASE.replace(
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => console.log('DB connections successful! '));
 
